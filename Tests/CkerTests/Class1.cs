e794e246a238aaaa32e5efc84b2d8bd0000c20f0
Robert Jakubowicz <robert@robert-jakubowicz.com>
2013-06-01 09:38:03 -0400

    New project structure and added NUnit

---
+++ Tests/CkerTests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using Cker;
+namespace CKerTests
+{
+    public class ServerTest
+    {
+        [Test]
+        public void TestMe() 
+        {
+            Server s = new Server();            
+        }
+    }
+}
