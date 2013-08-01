7a827e7d9412e94d172fa0b456b5a0ddb1bbffec
FireKraken <ashwath.george@gmail.com>
2013-08-01 14:54:37 -0400

    Added classes and resources for Login/Authentication

---
+++ Src/CkerGUI/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CkerGUI.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string EMailAddress { get; set; }
+        public string ImageSourcePath { get; set; }
+    }
+}
